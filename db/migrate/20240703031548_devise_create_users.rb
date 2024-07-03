8d584ef01e8127d4439dbdd60d49485467c3972f
suzu1996ko <0tj6892738t256x@gmail.com>
2024-07-03 12:36:42 +0900

    マイグレーションファイルの編集

--- db/migrate/20240703031548_devise_create_users.rb
+++ db/migrate/20240703031548_devise_create_users.rb
@@ -3,6 +3,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :username, null: false
+      t.string :name, null: false
+      t.text   :bio, null: false
+      t.date   :birthday, null: false
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
