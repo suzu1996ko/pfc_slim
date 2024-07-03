3e0d8abfdab93735201ce0d0e29f9158fda86aca
suzu1996ko <0tj6892738t256x@gmail.com>
2024-07-03 21:21:13 +0900

    physical_informationsマイグレーションファイルへの記述

--- db/migrate/20240703114600_create_physical_informations.rb
+++ db/migrate/20240703114600_create_physical_informations.rb
@@ -1,6 +1,13 @@
 class CreatePhysicalInformations < ActiveRecord::Migration[7.0]
   def change
     create_table :physical_informations do |t|
+      t.integer :age, null: false
+      t.integer :sex_id, null: false
+      t.integer :height, null: false
+      t.integer :weight, null: false
+      t.integer :age, null: false
+      t.integer :active_id, null: false
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
