ae3b90ec853a4098c7a6a842774ea8d885174f86
suzu1996ko <0tj6892738t256x@gmail.com>
2024-07-03 21:33:35 +0900

    PhysicalInformationモデルのバリデーションの記述

--- app/models/physical_information.rb
+++ app/models/physical_information.rb
@@ -1,2 +1,10 @@
 class PhysicalInformation < ApplicationRecord
+  belongs_to :user, optional: true
+  with_options presence: true do
+    validates :age
+    validates :sex_id
+    validates :height
+    validates :weight
+    validates :active_id
+  end
 end
