4b8a3903eafb8bc8a0dac5c6c1a28f10344941fc
suzu1996ko <0tj6892738t256x@gmail.com>
2024-07-04 20:58:51 +0900

    身体情報の表示と登録をするルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'physical_informations', to: 'users/registrations#new_physical_information'
+    post 'physical_informations', to: 'users/registrations#create_physical_information'
+  end
   root to: "home#index"
 end
